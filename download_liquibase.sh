ed0f85a605dfbfd3aae7ce3e39bae521d4772925
Tsvi Zandany <44092058+szandany@users.noreply.github.com>
2021-12-13 09:15:14 -0600

    Update and rename download_liquibase.sh to update_homebrew_liquibase.sh

--- download_liquibase.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-VERSION="${1:-"$(curl -s https://github.com/liquibase/liquibase/releases/latest | grep -o "v.*" | sed s/'>.*'//g |  sed s/'v'//g | sed 's/"//g')"}"
-curl -L https://github.com/liquibase/liquibase/releases/download/v${VERSION}/liquibase-${VERSION}.tar.gz --output liquibase-${VERSION}.tar.gz
