e4936252912821271a34c00bfd3aa7bbbe10ac59
Tsvi Zandany <44092058+szandany@users.noreply.github.com>
2021-12-13 08:35:29 -0600

    Update download_liquibase.sh

--- download_liquibase.sh
+++ download_liquibase.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 VERSION="${1:-"$(curl -s https://github.com/liquibase/liquibase/releases/latest | grep -o "v.*" | sed s/'>.*'//g |  sed s/'v'//g | sed 's/"//g')"}"
-curl -L https://github.com/liquibase/liquibase/releases/download/v${VERSION}/liquibase-${VERSION}.zip --output liquibase-${VERSION}.zip
+curl -L https://github.com/liquibase/liquibase/releases/download/v${VERSION}/liquibase-${VERSION}.tar.gz --output liquibase-${VERSION}.tar.gz
