c4f45dd0530b7a1cb5d69151709584b15c91d77d
Tsvi Zandany <44092058+szandany@users.noreply.github.com>
2021-12-13 08:31:56 -0600

    Create download_liquibase.sh

---
+++ download_liquibase.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+VERSION="${1:-"$(curl -s https://github.com/liquibase/liquibase/releases/latest | grep -o "v.*" | sed s/'>.*'//g |  sed s/'v'//g | sed 's/"//g')"}"
+curl -L https://github.com/liquibase/liquibase/releases/download/v${VERSION}/liquibase-${VERSION}.zip --output liquibase-${VERSION}.zip
